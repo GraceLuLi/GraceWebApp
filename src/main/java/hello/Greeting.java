beaa0d5b575c45520c294b854d97b3cfee4a98d6
Grace Lu Li <lu.li.azure@gmail.com>
2015-11-10 22:52:47 -0800

    test SSH key

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,11 +4,12 @@ public class Greeting {
 
     private final long id;
     private final String content;
+    private final String testSSH;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
-
+        this.testSSH = "";
     }
 
     public long getId() {
